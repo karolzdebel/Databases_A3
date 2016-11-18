6070eca04f205dbc0a9692feaf932cdf562d8883
PostgreSQL administrator <kzdebel@mail.uoguelph.ca>
2016-11-18 04:42:31 -0500

    finished all files still making minor changes

---
+++ f08.sql
@@ -0,0 +1,30 @@
+CREATE OR REPLACE FUNCTION f08(tNum text, vNum text, accNum text, amt real) RETURNS integer AS $$
+DECLARE
+	cust customer%ROWTYPE;
+	vend vendor%ROWTYPE;
+	newTrans transaction%ROWTYPE;	
+BEGIN
+	
+	newTrans := (tNum,vNum,accNum,CURRENT_DATE,amt);
+
+	INSERT INTO transaction VALUES (newTrans);
+
+	UPDATE customer SET Cbalance = Cbalance - amt
+		WHERE accNum = Account; 
+
+	UPDATE vendor SET Vbalance = Vbalance + amt
+		WHERE Vno = vNum;
+
+	SELECT * into cust FROM customer 
+		WHERE Account = accNum;
+
+	SELECT * into vend FROM vendor
+		WHERE Vno = vNum;
+
+	raise notice 'New transaction: %',newTrans;
+	raise notice 'Customer %(Account %) sent % to vendor %(Vno %). Customer updated balance is %. Vendor updated balance is %.',
+		cust.Cname,cust.Account,amt,vend.Vname,vend.Vno,cust.Cbalance,vend.Vbalance;	
+	
+	RETURN 0;
+END;
+$$ LANGUAGE plpgsql;
